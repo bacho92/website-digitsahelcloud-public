969d831a515e4e44b57a3893930cd3cd4bed0e15
Bachir Abourahamane Aboubacar <admin-dsc@digitsahelcloud.com>
2026-04-15 08:47:23 +0100

    feat: SSL nginx 443 + securite headers + modeles contact

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $fillable = [
+        'demandeur_type',
+        'company',
         'name',
         'email',
         'phone',
+        'role',
         'service',
         'message',
         'is_read',
